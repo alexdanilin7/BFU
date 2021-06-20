d74b62cd6ca68d9f61c4cfffbadf98edfa60c262
Mikhail <joinmma@gmail.com>
2021-06-20 12:56:26 +0200

    without fetch

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,12 @@ require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-const APPLICANTS_URL = process.env.APPLICANTS_URL
-const RATING_URL = process.env.RATING_URL
-const EXAM_URL = process.env.EXAM_URL
+// const APPLICANTS_URL = process.env.APPLICANTS_URL
+// const RATING_URL = process.env.RATING_URL
+// const EXAM_URL = process.env.EXAM_URL
 const FETCH_INTERVAL = parseInt(process.env.FETCH_INTERVAL || '300000')
-const LOGIN = process.env.LOGIN
-const PASSWORD = process.env.PASSWORD
+// const LOGIN = process.env.LOGIN
+// const PASSWORD = process.env.PASSWORD
 
 app.use('/static', express.static(path.join(__dirname, '../', 'static')))
 app.use(express.json())
@@ -33,24 +33,22 @@ app.set('views', path.join(__dirname, '../', 'templates'))
 app.listen(PORT, () => console.log(`Server has been started on port ${PORT}!`))
 
 setInterval(async () => {
-  let resp = await fetch(APPLICANTS_URL!, {
-    headers: {
-      Authorization: 'Basic' + base64.encode(LOGIN! + ':' + PASSWORD!),
-    },
-  })
-  let data = await resp.json()
-  writeFileSync(
-    path.join(__dirname, '../', 'static', 'applicants.json'),
-    JSON.stringify(parseApplicantsList(data))
-  )
-
+  // let resp = await fetch(APPLICANTS_URL!, {
+  //   headers: {
+  //     Authorization: 'Basic' + base64.encode(LOGIN! + ':' + PASSWORD!),
+  //   },
+  // })
+  // let data = await resp.json()
+  // writeFileSync(
+  //   path.join(__dirname, '../', 'static', 'applicants.json'),
+  //   JSON.stringify(parseApplicantsList(data))
+  // )
   // resp = await fetch(RATING_URL!)
   // data = await resp.json()
   // writeFileSync(
   //   path.join(__dirname, '../', 'static', 'rating.json'),
   //   JSON.stringify(parseRatingList(data))
   // )
-
   // resp = await fetch(EXAM_URL!)
   // data = await resp.json()
   // writeFileSync(
