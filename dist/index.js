d8f97273a748a98662dbd4271e28b0e2c6445d5a
Mikhail <joinmma@gmail.com>
2021-06-21 10:04:06 +0200

    without api

--- dist/index.js
+++ dist/index.js
@@ -1,63 +1,23 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var path_1 = __importDefault(require("path"));
-var node_fetch_1 = __importDefault(require("node-fetch"));
-var base_64_1 = __importDefault(require("base-64"));
 var routes_1 = __importDefault(require("./applicants/routes"));
 var routes_2 = __importDefault(require("./client/routes"));
 var routes_3 = __importDefault(require("./exam/routes"));
 var routes_4 = __importDefault(require("./rating/routes"));
-var fs_1 = require("fs");
-var service_1 = require("./service");
 require('dotenv').config();
 var app = express_1.default();
 var PORT = process.env.PORT || 5000;
-var APPLICANTS_URL = process.env.APPLICANTS_URL;
+// const APPLICANTS_URL = process.env.APPLICANTS_URL
 // const RATING_URL = process.env.RATING_URL
 // const EXAM_URL = process.env.EXAM_URL
-var FETCH_INTERVAL = parseInt(process.env.FETCH_INTERVAL || '300000');
-var LOGIN = process.env.LOGIN;
-var PASSWORD = process.env.PASSWORD;
+// const FETCH_INTERVAL = parseInt(process.env.FETCH_INTERVAL || '300000')
+// const LOGIN = process.env.LOGIN
+// const PASSWORD = process.env.PASSWORD
 app.use('/static', express_1.default.static(path_1.default.join(__dirname, '../', 'static')));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
@@ -68,22 +28,27 @@ app.use('/', routes_2.default);
 app.set('view engine', 'ejs');
 app.set('views', path_1.default.join(__dirname, '../', 'templates'));
 app.listen(PORT, function () { return console.log("Server has been started on port " + PORT + "!"); });
-setInterval(function () { return __awaiter(void 0, void 0, void 0, function () {
-    var resp, data;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, node_fetch_1.default(APPLICANTS_URL, {
-                    headers: {
-                        Authorization: 'Basic' + base_64_1.default.encode(LOGIN + ':' + PASSWORD),
-                    },
-                })];
-            case 1:
-                resp = _a.sent();
-                return [4 /*yield*/, resp.json()];
-            case 2:
-                data = _a.sent();
-                fs_1.writeFileSync(path_1.default.join(__dirname, '../', 'static', 'applicants.json'), JSON.stringify(service_1.parseApplicantsList(data)));
-                return [2 /*return*/];
-        }
-    });
-}); }, FETCH_INTERVAL);
+// setInterval(async () => {
+//   let resp = await fetch(APPLICANTS_URL!, {
+//     headers: {
+//       Authorization: 'Basic' + base64.encode(LOGIN! + ':' + PASSWORD!),
+//     },
+//   })
+//   let data = await resp.json()
+//   writeFileSync(
+//     path.join(__dirname, '../', 'static', 'applicants.json'),
+//     JSON.stringify(parseApplicantsList(data))
+//   )
+//   resp = await fetch(RATING_URL!)
+//   data = await resp.json()
+//   writeFileSync(
+//     path.join(__dirname, '../', 'static', 'rating.json'),
+//     JSON.stringify(parseRatingList(data))
+//   )
+//   resp = await fetch(EXAM_URL!)
+//   data = await resp.json()
+//   writeFileSync(
+//     path.join(__dirname, '../', 'static', 'exam.json'),
+//     JSON.stringify(parseExamList(data))
+//   )
+// }, FETCH_INTERVAL)
